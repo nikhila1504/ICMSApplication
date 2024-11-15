bb87074e8cffc14f5c936c5f334aed64578e9d0a
Ajay <gonea@sbwc.ga.gov>
2024-11-15 12:02:27 -0500

    Reports-Changes

--- src/main/java/com/icms/party/controller/WC1FormController.java
+++ src/main/java/com/icms/party/controller/WC1FormController.java
@@ -26,6 +26,7 @@ import com.icms.party.entity.InjuryType;
 import com.icms.party.entity.NaicsType;
 import com.icms.party.entity.StateType;
 import com.icms.party.entity.TreatmentType;
+import com.icms.party.entity.UserProductivityReport;
 import com.icms.party.entity.Wc1Form;
 import com.icms.party.service.BodyPartTypeServiceImpl;
 import com.icms.party.service.ClaimPartyServiceImpl;
@@ -38,6 +39,7 @@ import com.icms.party.service.InjuryTypeServiceImpl;
 import com.icms.party.service.NaicsTypeServiceImpl;
 import com.icms.party.service.StateTypeServiceImpl;
 import com.icms.party.service.TreatmentTypeServiceImpl;
+import com.icms.party.service.UserProductivityReportServiceImpl;
 import com.icms.party.service.Wc1FormServiceImpl;
 
 import io.swagger.annotations.Api;
@@ -84,6 +86,9 @@ public class WC1FormController {
 
 	@Autowired
 	private DisabilityTypeServiceImpl disabilityTypeServiceImpl;
+	
+	@Autowired
+	private UserProductivityReportServiceImpl userProductivityReportServiceImpl;
 
 	public Claim loadClaimByClaimNo(String claimNo) throws UsernameNotFoundException {
 		claimNo = "2024000100";
@@ -230,6 +235,13 @@ public class WC1FormController {
 	@PreAuthorize("hasRole('ADMIN')or hasRole('USER')")
 	public ClaimParty getClaimPartyByById(@PathVariable Long claimPartyId) {
 		return claimPartyService.getClaimPartyById(claimPartyId);
+	}
+	
+	@GetMapping(value = "/userProductiveReport")
+	@ApiOperation(value = "get user ProductiveReport")
+	@PreAuthorize("hasRole('ADMIN')or hasRole('USER')")
+	public List<UserProductivityReport> getUserProductiveReport() {
+		return userProductivityReportServiceImpl.getUserProductivityReport();
 	}
 
 }
---
+++ src/main/java/com/icms/party/service/UserProductivityReportServiceImpl.java
@@ -0,0 +1,27 @@
+package com.icms.party.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import com.icms.party.entity.UserProductivityReport;
+import com.icms.party.repository.UserProductivityReportRepository;
+
+@Service
+public class UserProductivityReportServiceImpl {
+	
+	@Autowired
+	private UserProductivityReportRepository userProductivityReportRepository;
+
+	public List<UserProductivityReport> getUserProductivityReport() {
+		 Pageable pageable = PageRequest.of(0, 500,Sort.by(Sort.Order.desc("userProductivityReportPK.activityDate"))); // 0 is the page number (first page), 500 is the page size
+	        Page<UserProductivityReport> page = userProductivityReportRepository.findAll(pageable);
+	        return page.getContent();
+	}
+
+}
